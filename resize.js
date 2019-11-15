ddc1f3894e8fb82f2e5849c909ed98dfdc02936d
klpoland1 <45948479+klpoland1@users.noreply.github.com>
2019-11-15 16:08:57 -0500

    Update resize.js

--- resize.js
+++ resize.js
@@ -1,7 +1,16 @@
 function getDocHeight(doc) {
 	doc = doc || document;
-	var body = doc.body, html = doc.documentElement;
-	var height = Math.max( body.scrollHeight, body.offsetHeight, 
-		html.clientHeight, html.scrollHeight, html.offsetHeight );
+	var body = doc.body;
+	var html = doc.documentElement;
+	var height = Math.max(body.scrollHeight, body.offsetHeight, 
+		html.clientHeight, html.scrollHeight, html.offsetHeight);
 	return height;
 }
+
+function resizeIframe(id) {
+    	var ifrm = document.getElementById(id);
+    	var doc = ifrm.contentDocument || ifrm.contentWindow.document;
+    	ifrm.style.visibility = 'hidden';
+    	ifrm.style.height = "10px";
+    	ifrm.style.height = getDocHeight(doc) + 4 + "px";
+    	ifrm.style.visibility = 'visible';
